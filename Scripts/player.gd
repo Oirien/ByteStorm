1a01264d4c8f4c810bde2aa2f936581c7c9fb3b8
oirien <robert.mayo.ch@gmail.com>
2023-10-09 16:54:15 +0100

    day_1 pushes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,4 +8,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = get_global_mouse_position()
+	var xDirection = 0
+	var yDirection = 0
+	
+	if (Input.is_action_pressed("ui_down")):
+		yDirection = 2
+	if (Input.is_action_pressed("ui_up")):
+		yDirection = -2
+	if (Input.is_action_pressed("ui_right")):
+		xDirection = 2
+	if (Input.is_action_pressed("ui_left")):
+		xDirection = -2
+	self.position += Vector2(xDirection, yDirection)
