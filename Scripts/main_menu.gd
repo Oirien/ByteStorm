063abee04c51436dd79628812142b5303ecc414f
oirien <robert.mayo.ch@gmail.com>
2023-10-12 18:57:44 +0100

    fixed main menu again

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
 
-
+func _ready():
+	pass
+	
+	
+func _process(_delta):
+	pass
 
 func _on_start_pressed():
-#	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
 	var level_1 = preload("res://Scenes/level_1.tscn").instantiate()
 	get_parent().add_child(level_1)
 	self.hide()
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,10 +1,10 @@
 extends Control
-var main_menu = preload("res://Scenes/main_menu.tscn").instantiate()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.hide()
-
+	#var main_menu = preload("res://Scenes/main_menu.tscn").instantiate()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
