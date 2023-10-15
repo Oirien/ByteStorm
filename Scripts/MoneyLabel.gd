2dda9d8494f2bcdd22833b20675c7e537b34ad79
James Dickson <jamesstuartdickson@gmail.com>
2023-10-15 17:06:11 +0100

    made a start on shop scene/scripts

---
+++ Scripts/MoneyLabel.gd
@@ -0,0 +1,13 @@
+extends RichTextLabel
+#@onready var PlayerDataNode = get_tree().get_root().get_node("Game").get_child(1)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+#	self.clear()
+#	self.add_text("Score " + str(PlayerDataNode._get_score()))
---
+++ Scripts/shop.gd
@@ -0,0 +1,10 @@
+extends Control
+#@onready var PlayerDataNode = get_tree().get_root().get_node("Game").get_child(1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	self.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
