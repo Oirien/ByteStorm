cb3e5973b88f4940bec977942583b0710516f6b1
James Dickson <jamesstuartdickson@gmail.com>
2023-10-16 14:50:54 +0100

    settings menu can now be accessed in game

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,5 +1,5 @@
 extends Control
-
+@onready var gamescene = get_node("../../")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,10 +16,15 @@ func _on_resume_pressed():
 
 
 func _on_quit_pressed():
-	var gamescene = get_node("../../")
-	
 	
 	gamescene.get_child(0).show()
 	gamescene.get_child(0).get_node("AudioStreamPlayer").play()
 	gamescene.get_child(4).queue_free()
 	Engine.time_scale = 1
+
+
+func _on_button_pressed():
+	print(gamescene)
+	gamescene.get_child(4).hide()
+	gamescene.get_node("Settings").show()
+	
