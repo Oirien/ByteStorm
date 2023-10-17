94e23bd3e0dd94c2c0e3caff1c26c4e761f64093
oirien <robert.mayo.ch@gmail.com>
2023-10-17 13:44:27 +0100

    fixed bullets despawning whilst paused

--- Scripts/player_bullet.gd
+++ Scripts/player_bullet.gd
@@ -1,21 +1,19 @@
 extends Node2D
 @onready var PlayerDataNode = get_tree().get_root().get_node("Game").get_child(1)
 var speed = 900
-var spawntime = Time.get_ticks_msec()
+@onready var spawntime = get_tree().create_timer(15) 
 var damage = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.position += Vector2(10,0)
 	damage = PlayerDataNode._get_damage()
-
+	spawntime.connect("timeout", self.queue_free)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(spawntime.timeout)
 	self.position += Vector2(speed * delta,0).rotated(self.rotation)
 	if ($RayCast2D.is_colliding()):
 		var enemy_hit = $RayCast2D.get_collider()
 		enemy_hit.get_parent().on_hit(damage)
 		self.queue_free()
-	if (Time.get_ticks_msec() - spawntime > 15000):
-		self.queue_free()
-	
